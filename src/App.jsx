352914359af935d77904e79fa765b62b0652efc0
Utilit1s <fran@puntocero.agency>
2024-03-11 16:54:29 -0400

    Componente testimonio y organización del home

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ImageCard from './components/ImageCard';
 import ImageCard2 from './components/ImageCard2';
 import ListadoServicios from "./components/ListadoServicios";
 import Cta from "./components/Cta";
+import Testimonial from "./components/Testimonial";
 const App = () => {
 
   const serviciosData = [
@@ -40,6 +41,11 @@ const App = () => {
   return (
     <div>
       <Nav />
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="text-5xl font-bold my-10">Descubre nuestros servicios</h2>
+        <p className="mt-2 text-lg my-10">Facilitamos la gestión de tus recursos y contribuimos a tu crecimiento económico y personal</p>
+        <ListadoServicios servicios={serviciosData} />
+      </div>
       <ImageCard
         imageUrl="./src/assets/images/services1.png"
         title="Servicio eficiente"
@@ -52,12 +58,12 @@ const App = () => {
         text="Promover la educación financiera de los estudiantes universitarios, a través de charlas, talleres y capacitaciones sobre temas como el ahorro, la inversión y el uso responsable del crédito"
         buttonText="Conócenos"
       />
-
-      <div className="flex flex-col items-center justify-center">
-        <h2 className="text-5xl font-bold my-10">Descubre nuestros servicios</h2>
-        <p className="mt-2 text-lg my-10">Facilitamos la gestión de tus recursos y contribuimos a tu crecimiento económico y personal</p>
-        <ListadoServicios servicios={serviciosData} />
-      </div>
+      <Testimonial
+        nombre="Miguel Iglesias"
+        cargo="Estudiante UCLA"
+        texto="El Banco Universitario ha sido mi aliado financiero desde que ingresé a la universidad. Su enfoque en brindar soluciones financieras ágiles y eficientes realmente se refleja en la forma en que facilitan la gestión de mis recursos. La opción de transferencias entre estudiantes sin comisión ha simplificado mis transacciones, y la posibilidad de realizar depósitos y retiros en efectivo las 24 horas del día ha sido increíblemente conveniente."
+        fotoUrl="./src/assets/images/services2.png"
+      />
       <Cta 
       title="Crea una cuenta en nuestra banca en línea"
       buttonText="Registrarse"/>
