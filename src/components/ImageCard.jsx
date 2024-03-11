6a91a2538bbc0bdffb66277700de07e1d0387b95
Utilit1s <fran@puntocero.agency>
2024-03-10 20:07:36 -0400

    Componentes ImageCard e ImageCard2, agregados al App y pequeños ajustes en index.css para estilos generales

---
+++ src/components/ImageCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const ImageCard = ({ imageUrl, title, text, buttonText}) => {
+  return (
+    <div className="grid grid-cols-2 gap-4 m-10 flex items-center mx-20 my-40">
+    <div className="col-span-1">
+      {/* Imagen a la izquierda */}
+      <img src={imageUrl} alt="Imagen" className="w-4/5 h-auto mx-10" />
+    </div>
+      {/* Contenido a la derecha */}
+      <div className="col-span-1 flex flex-col justify-center mx-10">
+        {/* Título */}
+        <h2 className="text-5xl font-bold my-4">{title}</h2>
+
+        {/* Texto */}
+        <p className="mt-2 text-lg">{text}</p>
+
+        {/* Botón con icono */}
+        <button className="mt-4 font-bold text-teal-400 py-2 rounded-md flex items-center">
+          <span>{buttonText}</span>
+          <svg className="w-4 h-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
+        </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ImageCard;
