89374d4103eb8e84f991ffc467ad5fb4ef197e05
Utilit1s <fran@puntocero.agency>
2024-03-14 11:31:34 -0400

    Ajustes del componente testimonio, agregado el icono de quote e implementado

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import Icono from './Icono';
 
 const Testimonial = ({ nombre, cargo, texto, fotoUrl }) => {
   return (
-    <div className="flex items-center justify-center space-x-4 my-20 mx-32">
+    <div className="flex items-center justify-center space-x-4 my-20 mx-40">
       {/* Contenido del lado izquierdo */}
-      <div className="flex-shrink-0 w-1/4 text-center">
+      <div className="flex-shrink-0 w-1/8 text-center">
         <img src={fotoUrl} alt={nombre} className="w-16 h-16 rounded-full mx-auto" />
-        <div>
-          <p className="text-lg font-bold">{nombre}</p>
-          <p className="text-sm">{cargo}</p>
-        </div>
+      </div>
+      <div className="flex-shrink-0 w-2/8 text-center">
+        <p className="text-lg font-bold">{nombre}</p>
+        <p className="text-sm">{cargo}</p>
       </div>
 
       {/* Contenido del lado derecho */}
-      <div className="w-3/4">
+      <div className="flex-shrink-0 w-1/8 items-start">
+        <Icono nombre="fa-quote-left" color="#49BEB7" tamaño="2x" />
+      </div>
+      <div className="w-4/8">
         <p className="text-lg">{texto}</p>
       </div>
     </div>
