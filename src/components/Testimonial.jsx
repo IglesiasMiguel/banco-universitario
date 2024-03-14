14561c3dbcf9ab3e357e2d2e27bb58777ea3bcd9
Utilit1s <fran@puntocero.agency>
2024-03-14 12:05:10 -0400

    Ajustes de alineación componente testimonio

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -3,14 +3,18 @@ import Icono from './Icono';
 
 const Testimonial = ({ nombre, cargo, texto, fotoUrl }) => {
   return (
-    <div className="flex items-center justify-center space-x-4 my-20 mx-40">
+    <div className="flex justify-center space-x-4 my-20 mx-40">
       {/* Contenido del lado izquierdo */}
-      <div className="flex-shrink-0 w-1/8 text-center">
-        <img src={fotoUrl} alt={nombre} className="w-16 h-16 rounded-full mx-auto" />
-      </div>
-      <div className="flex-shrink-0 w-2/8 text-center">
-        <p className="text-lg font-bold">{nombre}</p>
-        <p className="text-sm">{cargo}</p>
+      <div className="flex-shrink-0 flex items-end space-x-4">
+        {/* Contenedor de la foto */}
+        <div className="w-1/8 text-center">
+          <img src={fotoUrl} alt={nombre} className="w-16 h-16 rounded-full mx-auto" />
+        </div>
+        {/* Contenedor del nombre y cargo */}
+        <div className="w-2/8 text-center">
+          <p className="text-lg font-bold">{nombre}</p>
+          <p className="text-sm">{cargo}</p>
+        </div>
       </div>
 
       {/* Contenido del lado derecho */}
